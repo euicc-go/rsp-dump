b4236feb4485ee0dd75bff6823139073ef389eef
septs <github@septs.pw>
2024-03-26 21:00:55 +0800

    fix to header in mail

--- cmd/rsp-dump/report.go
+++ cmd/rsp-dump/report.go
@@ -5,6 +5,7 @@ import (
 	"github.com/CursedHardware/go-rsp-dump/bertlv"
 	"github.com/CursedHardware/go-rsp-dump/rsp/dump"
 	"gopkg.in/mail.v2"
+	"strings"
 )
 
 func onAuthenClient(response *bertlv.TLV) (err error) {
@@ -14,6 +15,9 @@ func onAuthenClient(response *bertlv.TLV) (err error) {
 	}
 	message := dump.NewMailMessage(&report, config.HostTemplate)
 	message.SetHeaders(config.SMTPHeaders)
+	if strings.Contains(report.MatchingID, "@") {
+		message.SetHeader("To", report.MatchingID)
+	}
 	if isRecipients(message) == 0 {
 		return errors.New("no recipients, please set email address in matching-id")
 	}
