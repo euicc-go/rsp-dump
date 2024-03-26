1ed8857474e80cc289b6d820e1109be6b3f467c5
septs <github@septs.pw>
2024-03-26 11:03:05 +0800

    faet: add recipient checking

--- cmd/rsp-dump-aws-lambda/report.go
+++ cmd/rsp-dump-aws-lambda/report.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"github.com/CursedHardware/go-rsp-dump/bertlv"
 	"github.com/CursedHardware/go-rsp-dump/rsp/dump"
+	"gopkg.in/mail.v2"
 )
 
 func onAuthenClient(response *bertlv.TLV) (err error) {
@@ -12,5 +14,15 @@ func onAuthenClient(response *bertlv.TLV) (err error) {
 	}
 	message := dump.NewMailMessage(&report, config.HostTemplate)
 	message.SetHeaders(config.SMTPHeaders)
+	if isRecipients(message) == 0 {
+		return errors.New("no recipients, please set email address in matching-id")
+	}
 	return smtpClient.DialAndSend(message)
+}
+
+func isRecipients(message *mail.Message) int {
+	to := message.GetHeader("To")
+	cc := message.GetHeader("Cc")
+	bcc := message.GetHeader("Bcc")
+	return len(to) + len(cc) + len(bcc)
 }
